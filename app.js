48c5f94595f7609308d074dd02e0f75b1f767f35
darecallad <Ashane199080706@hotmail.com.tw>
2021-08-12 14:56:47 -0700

    Http Module

--- app.js
+++ app.js
@@ -62,3 +62,24 @@ const Logger = require("./logger");
 const logger = new Logger();
 
 logger.log("Test 123");
+
+//HTTp Module
+const http = require("http");
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.write("Hello World");
+    res.end();
+  }
+
+  if (req.url === "/api/courses") {
+    res.write(JSON.stringify([1, 2, 3]));
+    res.end();
+  }
+});
+
+// server.on("connection", (socket) => {
+//   console.log("New Connection");
+// });
+
+server.listen(3000);
+console.log("listen on port 3000...");
