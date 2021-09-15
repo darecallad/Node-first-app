d81dc4ad555aa262bd0607015224bcda0895009f
darecallad <Ashane199080706@hotmail.com.tw>
2021-09-15 10:04:07 -0700

    review

--- app.js
+++ app.js
@@ -1,85 +1,20 @@
-// function sayHello(name) {
-//   console.log("Hello" + name);
-// }
-
-// const logger = require("./logger");
-// console.log(logger);
-
-// logger.log("message");
-
-// window obj
-// console.log() //global
-// setTimeout(); // call function after delay
-// clearTimeout();
-// setInterval();
-// clearInterval();
-// var
-// global.console.log();
-
-//Path Module
-// const path = require("path");
-// var pathObj = path.parse(__filename);
-// console.log(pathObj);
-
-// //OS Module
-
-// const os = require("os");
-
-// var totalMemory = os.totalmem();
-// var freeMemory = os.freemem();
-
-// console.log("Total Memory: " + totalMemory);
-
-// console.log(`Total Memory: ${totalMemory}`);
-// console.log(`free Memory: ${freeMemory}`);
-
-// // File System Module
-
-// const fs = require("fs");
-// const file = fs.readdirSync("./");
-// console.log(file);
-
-// fs.readdir("./", function (ex, files) {
-//   if (ex) console.log("Error", ex);
-//   else console.log("Result", files);
-// });
-
-//Event Module
-// const EventEmitter = require("events"); //Class
-
-//Order is important
-
-// const Logger = require("./logger");
-// const logger = new Logger();
-
-// //Register a listener
-// logger.on("messageLogged", (arg) => {
-//   console.log("Listener call", arg);
-// });
-// logger.log("message");
-
 const Logger = require("./logger");
-const logger = new Logger();
 
-logger.log("Test 123");
+// Module System
+// Node Package NPM
+// RESTFUl API
+// Async JS
+// Storing Data in MongoDB
+// Authentication and Authorization
+// Handling and Logging Error
+// Unit Testing
+// Test Driven Development TDD
+// Deployment
 
-//HTTp Module
-const http = require("http");
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.write("Hello World");
-    res.end();
-  }
+// console.log(module);
+const log = new Logger();
 
-  if (req.url === "/api/courses") {
-    res.write(JSON.stringify([1, 2, 3]));
-    res.end();
-  }
+log.on("messageLogged", (arg) => {
+  console.log("Listener called", arg);
 });
-
-// server.on("connection", (socket) => {
-//   console.log("New Connection");
-// });
-
-server.listen(3000);
-console.log("listen on port 3000...");
+log.log("message");
--- logger.js
+++ logger.js
@@ -4,7 +4,9 @@ const EventEmitter = require("events"); //Class
 class Logger extends EventEmitter {
   log(message) {
     console.log(message);
+    this.emit("messageLogged", { id: 1 });
   }
 }
 
 module.exports = Logger;
+// console.log(module);
